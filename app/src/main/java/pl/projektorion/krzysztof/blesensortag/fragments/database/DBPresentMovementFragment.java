7819f63221503995da37adc5b23ee2830bdc9123
Krzysztof Kruk <krzysztof.pawel.kruk@gmail.com>
2016-12-25 21:01:27 +0100

    Added movement presentation fragment * added labels for axes * added horizontal layout for movement * interactivity to be implemented soon. Today's coding streak is more than enough

---
+++ app/src/main/java/pl/projektorion/krzysztof/blesensortag/fragments/database/DBPresentMovementFragment.java
@@ -0,0 +1,333 @@
+package pl.projektorion.krzysztof.blesensortag.fragments.database;
+
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.os.Handler;
+import android.support.v4.os.ResultReceiver;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import pl.projektorion.krzysztof.blesensortag.R;
+import pl.projektorion.krzysztof.blesensortag.database.DBSelectIntentService;
+import pl.projektorion.krzysztof.blesensortag.database.commands.DBQueryParcelableListenerInterface;
+import pl.projektorion.krzysztof.blesensortag.database.commands.DBQueryWithLimitsListenerInterface;
+import pl.projektorion.krzysztof.blesensortag.database.selects.DBSelectInterface;
+import pl.projektorion.krzysztof.blesensortag.database.selects.Movement.DBSelectMovement;
+import pl.projektorion.krzysztof.blesensortag.database.selects.Movement.DBSelectMovementCount;
+import pl.projektorion.krzysztof.blesensortag.database.selects.Movement.DBSelectMovementCountData;
+import pl.projektorion.krzysztof.blesensortag.database.selects.Movement.DBSelectMovementData;
+import pl.projektorion.krzysztof.blesensortag.utils.ServiceDataReceiver;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DBPresentMovementFragment extends DBPresentSensorFragmentAbstract {
+
+    private View view;
+    private LineChart accChart;
+    private LineDataSet accXSet;
+    private LineDataSet accYSet;
+    private LineDataSet accZSet;
+
+    private LineChart gyrChart;
+    private LineDataSet gyrXSet;
+    private LineDataSet gyrYSet;
+    private LineDataSet gyrZSet;
+
+    private LineChart magChart;
+    private LineDataSet magXSet;
+    private LineDataSet magYSet;
+    private LineDataSet magZSet;
+
+
+    private ServiceDataReceiver.ReceiverListener receiverListener = new ServiceDataReceiver.ReceiverListener() {
+        @Override
+        public void onReceiveResult(int resultCode, Bundle resultData) {
+            if( resultCode == DBSelectIntentService.EXTRA_RESULT_CODE )
+            {
+                List<? extends DBSelectInterface> data = resultData
+                        .getParcelableArrayList(DBSelectIntentService.EXTRA_RESULT);
+                if( data == null ) return;
+                apply_data(data);
+            }
+        }
+    };
+
+    public DBPresentMovementFragment() {
+        // Required empty public constructor
+    }
+
+    public static DBPresentMovementFragment newInstance(DBSelectInterface rootRecord, DBSelectInterface sensorRecord) {
+        Bundle args = new Bundle();
+        args.putParcelable(EXTRA_ROOT_RECORD, rootRecord);
+        args.putParcelable(EXTRA_SENSOR_RECORD, sensorRecord);
+        DBPresentMovementFragment fragment = new DBPresentMovementFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_dbpresent_movement, container, false);
+        init_widgets();
+        return view;
+    }
+
+    @Override
+    public void onReceiveResult(int resultCode, Bundle resultData) {
+        super.onReceiveResult(resultCode, resultData);
+    }
+
+    @Override
+    protected DBQueryParcelableListenerInterface data_counter_instance() {
+        return new DBSelectMovementCount(rootRecord);
+    }
+
+    @Override
+    protected int attribute_count() {
+        return DBSelectMovementCountData.ATTRIBUTE_COUNT;
+    }
+
+    @Override
+    protected ResultReceiver data_receiver() {
+        ServiceDataReceiver dataReceiver = new ServiceDataReceiver(new Handler());
+        dataReceiver.setListener(receiverListener);
+        return dataReceiver;
+    }
+
+    @Override
+    protected DBQueryParcelableListenerInterface data_instance() {
+        DBQueryWithLimitsListenerInterface movementQuery = new DBSelectMovement(
+                rootRecord, sensorRecord);
+        movementQuery.setLimit(super.getStartAt(), super.getMaxRecordsPerLoad());
+        return movementQuery;
+    }
+
+    @Override
+    protected void apply_data(List<? extends DBSelectInterface> data) {
+        List<Entry> accXData = new ArrayList<>();
+        List<Entry> accYData = new ArrayList<>();
+        List<Entry> accZData = new ArrayList<>();
+
+        List<Entry> gyrXData = new ArrayList<>();
+        List<Entry> gyrYData = new ArrayList<>();
+        List<Entry> gyrZData = new ArrayList<>();
+
+        List<Entry> magXData = new ArrayList<>();
+        List<Entry> magYData = new ArrayList<>();
+        List<Entry> magZData = new ArrayList<>();
+
+        for(DBSelectInterface record : data)
+        {
+            final long time = (long) record.getData(DBSelectMovementData.ATTRIBUTE_MEASUREMENT);
+            accXData.add(new Entry(time, (float) record.getData(DBSelectMovementData.ATTRIBUTE_ACC_X)));
+            accYData.add(new Entry(time, (float) record.getData(DBSelectMovementData.ATTRIBUTE_ACC_Y)));
+            accZData.add(new Entry(time, (float) record.getData(DBSelectMovementData.ATTRIBUTE_ACC_Z)));
+
+            gyrXData.add(new Entry(time, (float) record.getData(DBSelectMovementData.ATTRIBUTE_GYRO_X)));
+            gyrYData.add(new Entry(time, (float) record.getData(DBSelectMovementData.ATTRIBUTE_GYRO_Y)));
+            gyrZData.add(new Entry(time, (float) record.getData(DBSelectMovementData.ATTRIBUTE_GYRO_Z)));
+
+            magXData.add(new Entry(time, (float) record.getData(DBSelectMovementData.ATTRIBUTE_MAGNET_X)));
+            magYData.add(new Entry(time, (float) record.getData(DBSelectMovementData.ATTRIBUTE_MAGNET_Y)));
+            magZData.add(new Entry(time, (float) record.getData(DBSelectMovementData.ATTRIBUTE_MAGNET_Z)));
+        }
+
+        create_acc_x_set(accXData);
+        create_acc_y_set(accYData);
+        create_acc_z_set(accZData);
+
+        create_gyr_x_set(gyrXData);
+        create_gyr_y_set(gyrYData);
+        create_gyr_z_set(gyrZData);
+
+        create_mag_x_set(magXData);
+        create_mag_y_set(magYData);
+        create_mag_z_set(magZData);
+
+        apply_to_acc_chart();
+        apply_to_gyr_chart();
+        apply_to_mag_chart();
+    }
+
+    private void init_widgets()
+    {
+        accChart = (LineChart) view.findViewById(R.id.acc_present_chart);
+        gyrChart = (LineChart) view.findViewById(R.id.gyr_present_chart);
+        magChart = (LineChart) view.findViewById(R.id.mag_present_chart);
+
+        setup_charts();
+    }
+
+    private void setup_charts()
+    {
+        accChart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
+        gyrChart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
+        magChart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
+
+        accChart.getDescription().setEnabled(false);
+        gyrChart.getDescription().setEnabled(false);
+        magChart.getDescription().setEnabled(false);
+    }
+
+    private void create_acc_x_set(List<Entry> data)
+    {
+        final String label = getString(R.string.axis_acc_x);
+        final int color = Color.RED;
+
+        accXSet = new LineDataSet(data, label);
+        accXSet.setDrawCircleHole(false);
+        accXSet.setDrawCircles(true);
+        accXSet.setCircleColor(color);
+        accXSet.setColor(color);
+    }
+
+    private void create_acc_y_set(List<Entry> data)
+    {
+        final String label = getString(R.string.axis_acc_y);
+        final int color = Color.GREEN;
+
+        accYSet = new LineDataSet(data, label);
+        accYSet.setDrawCircleHole(false);
+        accYSet.setDrawCircles(true);
+        accYSet.setCircleColor(color);
+        accYSet.setColor(color);
+    }
+
+    private void create_acc_z_set(List<Entry> data)
+    {
+        final String label = getString(R.string.axis_acc_z);
+        final int color = Color.BLUE;
+
+        accZSet = new LineDataSet(data, label);
+        accZSet.setDrawCircleHole(false);
+        accZSet.setDrawCircles(true);
+        accZSet.setCircleColor(color);
+        accZSet.setColor(color);
+    }
+
+    private void create_gyr_x_set(List<Entry> data)
+    {
+        final String label = getString(R.string.axis_gyr_x);
+        final int color = Color.RED;
+
+        gyrXSet = new LineDataSet(data, label);
+        gyrXSet.setDrawCircleHole(false);
+        gyrXSet.setDrawCircles(true);
+        gyrXSet.setCircleColor(color);
+        gyrXSet.setColor(color);
+    }
+
+    private void create_gyr_y_set(List<Entry> data)
+    {
+        final String label = getString(R.string.axis_gyr_y);
+        final int color = Color.GREEN;
+
+        gyrYSet = new LineDataSet(data, label);
+        gyrYSet.setDrawCircleHole(false);
+        gyrYSet.setDrawCircles(true);
+        gyrYSet.setCircleColor(color);
+        gyrYSet.setColor(color);
+    }
+
+    private void create_gyr_z_set(List<Entry> data)
+    {
+        final String label = getString(R.string.axis_gyr_z);
+        final int color = Color.BLUE;
+
+        gyrZSet = new LineDataSet(data, label);
+        gyrZSet.setDrawCircleHole(false);
+        gyrZSet.setDrawCircles(true);
+        gyrZSet.setCircleColor(color);
+        gyrZSet.setColor(color);
+    }
+
+    private void create_mag_x_set(List<Entry> data)
+    {
+        final String label = getString(R.string.axis_mag_x);
+        final int color = Color.RED;
+
+        magXSet = new LineDataSet(data, label);
+        magXSet.setDrawCircleHole(false);
+        magXSet.setDrawCircles(true);
+        magXSet.setCircleColor(color);
+        magXSet.setColor(color);
+    }
+
+    private void create_mag_y_set(List<Entry> data)
+    {
+        final String label = getString(R.string.axis_mag_y);
+        final int color = Color.GREEN;
+
+        magYSet = new LineDataSet(data, label);
+        magYSet.setDrawCircleHole(false);
+        magYSet.setDrawCircles(true);
+        magYSet.setCircleColor(color);
+        magYSet.setColor(color);
+    }
+
+    private void create_mag_z_set(List<Entry> data)
+    {
+        final String label = getString(R.string.axis_mag_z);
+        final int color = Color.BLUE;
+
+        magZSet = new LineDataSet(data, label);
+        magZSet.setDrawCircleHole(false);
+        magZSet.setDrawCircles(true);
+        magZSet.setCircleColor(color);
+        magZSet.setColor(color);
+    }
+
+    private void apply_to_acc_chart()
+    {
+        List<ILineDataSet> dataSets = new ArrayList<>();
+        dataSets.add(accXSet);
+        dataSets.add(accYSet);
+        dataSets.add(accZSet);
+
+        final LineData lineData = new LineData(dataSets);
+        accChart.setData(lineData);
+        accChart.invalidate();
+        accChart.notifyDataSetChanged();
+    }
+
+    private void apply_to_gyr_chart()
+    {
+        List<ILineDataSet> dataSets = new ArrayList<>();
+        dataSets.add(gyrXSet);
+        dataSets.add(gyrYSet);
+        dataSets.add(gyrZSet);
+
+        final LineData lineData = new LineData(dataSets);
+        gyrChart.setData(lineData);
+        gyrChart.invalidate();
+        gyrChart.notifyDataSetChanged();
+    }
+
+    private void apply_to_mag_chart()
+    {
+        List<ILineDataSet> dataSets = new ArrayList<>();
+        dataSets.add(magXSet);
+        dataSets.add(magYSet);
+        dataSets.add(magZSet);
+
+        final LineData lineData = new LineData(dataSets);
+        magChart.setData(lineData);
+        magChart.invalidate();
+        magChart.notifyDataSetChanged();
+    }
+}
