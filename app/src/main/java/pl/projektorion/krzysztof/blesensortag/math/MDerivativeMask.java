11c7cda7b27cafd9346f486a8e39b7b76fefbc4e
Krzysztof Kruk <krzysztof.pawel.kruk@gmail.com>
2017-01-17 17:39:44 +0100

    Added derivative masks

---
+++ app/src/main/java/pl/projektorion/krzysztof/blesensortag/math/MDerivativeMask.java
@@ -0,0 +1,18 @@
+package pl.projektorion.krzysztof.blesensortag.math;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by krzysztof on 17.01.17.
+ */
+
+public interface MDerivativeMask {
+    /*
+    Retrieved from https://youtu.be/SYVrLncRYlU?t=22m38s
+     */
+    List<Double> BACKWARD_DIFFERENCE = new ArrayList<>(Arrays.asList(-1.0, 1.0));
+    List<Double> CENTRAL_DIFFERENCE = new ArrayList<>(Arrays.asList(-1.0, 0.0, 1.0));
+    List<Double> FORWARD_DIFFERENCE = new ArrayList<>(Arrays.asList(1.0, -1.0));
+}
